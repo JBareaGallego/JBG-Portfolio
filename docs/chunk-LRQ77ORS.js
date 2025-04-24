a5a92c94932d690a7b94da9f80b4b7d25dd177ca
Juan Barea <jbarea282@gmail.com>
2025-04-24 12:47:58 +0200

    Github pages testing

---
+++ docs/chunk-LRQ77ORS.js
@@ -0,0 +1,2 @@
+import{Db as a,Wa as o,nb as s,ob as i}from"./chunk-ZCIREVLK.js";var n=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-english"]],decls:2,vars:0,template:function(e,l){e&1&&(s(0,"p"),a(1,"english works!"),i())},encapsulation:2})};export{n as default};
+/**i18n:41a2b9e0b583c72cd780fed429ab2bb1c71644a29777c5528b27141e9126bbc4*/
